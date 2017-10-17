931fa5e7b9a703c27556fe207f24d75f294d2e12
Ed Jefferson <e.jefferson@gmail.com>
2017-10-17 17:44:05 +0100

    displays artist graphs, tidied up data nav

--- db/migrate/20171005154056_add_castaway_aliases_to_castaways.rb
+++ db/migrate/20171005154056_add_castaway_aliases_to_castaways.rb
@@ -1,5 +1,4 @@
-class AddCastaway
-  AliasesToCastaways < ActiveRecord::Migration[5.1]
+class AddCastawayAliasesToCastaways < ActiveRecord::Migration[5.1]
   def change
     add_column :castaways, :castaway_aliases, :string, array: true
   end
